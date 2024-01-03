3f9ec50a2b212a51bdf2b204f1193f844e93563d
rocoso13 <kevinrsrojo@gmail.com>
2024-01-03 00:10:57 -0600

    Primer commit

---
+++ src/main/java/com/example/demo/BackendAplicacionApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAplicacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAplicacionApplication.class, args);
+	}
+
+}
